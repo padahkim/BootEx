c16ebae3982f02c2fe2b55943e8a31a4c7d4bbb9
padahkim <padahkim@gmail.com>
2022-05-31 23:39:12 +0900

    22.Many To One JPA exercise +native query and Test

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -1,3 +1,21 @@
 INSERT INTO article(id, title, content) VALUES (1, '가가가가', '1111');
 INSERT INTO article(id, title, content) VALUES (2, 'nanana', '2222');
 INSERT INTO article(id, title, content) VALUES (3, 'dadada', '3333');
+
+-- article 더미 데이터
+INSERT INTO article(id, title, content) VALUES (4, '당신의 인생영화는', '댓글ㄱ');
+INSERT INTO article(id, title, content) VALUES (5, '단신의 소울푸트', '댓글ㄱㄱ');
+INSERT INTO article(id, title, content) VALUES (6, '당신의 취미', '댓글ㄲㄱㄱ');
+
+--comment 더미 데이터
+INSERT INTO comment(id, article_id, nickname, body) VALUES (1,4, 'Park', '굿 윌 헌팅');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (2,4, 'Kim', '아이 엠 샘');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (3,4, 'Choi', '쇼생크탈출');
+
+INSERT INTO comment(id, article_id, nickname, body) VALUES (4,5, 'Park', '칰');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (5,5, 'Kim', 'ㅅㅄㅂ');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (6,5, 'Choi', 'ㅊㅂ');
+
+INSERT INTO comment(id, article_id, nickname, body) VALUES (7,6, 'Park', '조깅');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (8,6, 'Kim', '유튭');
+INSERT INTO comment(id, article_id, nickname, body) VALUES (9,6, 'Choi', '난파');
---
+++ firstproject/src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest //JPA와 연동한 테스트
+class CommentRepositoryTest {
+
+    @Autowired CommentRepository commentRepository;
+
+    //DiaplayName 이 테스트를 어떻게 보여줄거냐(테스트 결과에 보여줄 이름)
+    @Test
+    @DisplayName("특정 게시글의 모든 댓글 조회")
+    void findByArticleId() {
+        /* Case 1: 4번 게시글의 모든 댓글 조회*/
+        {
+            // 입력 데이터 준비
+                Long articleId = 4L;
+            // 실제 수행
+               List<Comment> comments = commentRepository.findByArticleId(articleId);
+            // 예상하기
+            Article article = new Article(4L, "당신의 인생영화는","댓글ㄱ");
+            Comment a = new Comment(1L,article,"Park","굿 윌 헌팅");
+            Comment b = new Comment(2L,article,"Kim","아이 엠 샘");
+            Comment c = new Comment(3L,article,"Choi","쇼생크탈출");
+
+            List<Comment> expected = Arrays.asList(a,b,c);
+
+            // 검증
+
+            assertEquals(expected.toString(), comments.toString(),"4번 글의 모든 댓글을 출력!");
+        }
+    }
+
+    /* Case 2: 1번 게시글의 모든 댓글 조회 (빈 상태로 만들어질것임 1번글을 댓글이 없음)*/
+
+    /* Case 3: 9번 게시글의 모든 댓글 조회 (빈 상태로 만들어질것임 1번글을 댓글이 없음)*/
+
+    /* Case 4: 9999번 게시글의 모든 댓글 조회 (빈 상태로 만들어질것임 1번글을 댓글이 없음)*/
+
+    /* Case 5: -1번 게시글의 모든 댓글 조회 (빈 상태로 만들어질것임 1번글을 댓글이 없음)*/
+    @Test
+    @DisplayName("특정 닉네임의 모든 댓글 조회")
+    void findByNickname() {
+        /* Case 1: "Park"의 모든 댓글 조회*/{
+
+            //입력 데이터 준비
+            String nickname="Park";
+            // 실제 수행
+              List<Comment> comments  = commentRepository.findByNickname(nickname);
+            // 예상하기
+
+            // 검증
+
+
+            /* Case 2: "Kim"의 모든 댓글 조회*/
+            /* Case 3: null의 모든 댓글 조회*/
+            /* Case 4: ""의 모든 댓글 조회*/
+            /* Case 5: "i"의 모든 댓글 조회*/
+
+        }
+    }
+}
